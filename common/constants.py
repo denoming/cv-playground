8dbcbed7a44f6f89fea8179d32592998e01efc3d
Denys Asauliak <denoming@gmail.com>
2024-04-26 17:57:49 +0300

    add supporting all image formats

--- common/constants.py
+++ common/constants.py
@@ -1,4 +1,8 @@
 from pathlib import Path
 
+# Path to dir with models
+CV_MODELS_DIR = Path.cwd().parent / 'models'
+# Path to dir with datasets
 CV_DATASETS_DIR = Path.cwd().parent / 'datasets'
+# Path to dir with project artifacts
 CV_WORKAREA_DIR = Path.cwd().parent / 'workarea'
