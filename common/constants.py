bcf230623282a5d0de17b58191f2c30a547b3a98
Denys Asauliak <denoming@gmail.com>
2024-10-27 14:28:20 +0200

    move file to separate dir

--- common/constants.py
+++ common/constants.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
+# Path to project root
+PROJECT_DIR = Path(__file__).parent.parent
 # Path to dir with models
-CV_MODELS_DIR = Path.cwd().parent / 'models'
+CV_MODELS_DIR = PROJECT_DIR / "models"
 # Path to dir with datasets
-CV_DATASETS_DIR = Path.cwd().parent / 'datasets'
+CV_DATASETS_DIR = PROJECT_DIR / "datasets"
 # Path to dir with project artifacts
-CV_WORKAREA_DIR = Path.cwd().parent / 'workarea'
+CV_WORKAREA_DIR = PROJECT_DIR / "workarea"
 # Path to dir with project artifacts
-CV_VIDEO_DIR = Path.cwd().parent / 'videos'
+CV_VIDEO_DIR = PROJECT_DIR / "videos"
