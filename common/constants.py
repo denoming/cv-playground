22ecc7ae8ba0dc682d5ba24c419d4529b10648dd
Denys Asauliak <denoming@gmail.com>
2026-06-12 16:32:32 +0300

    fix: update paths

--- common/constants.py
+++ common/constants.py
@@ -5,6 +5,6 @@ PROJECT_DIR = Path(__file__).parent.parent
 # Path to dir with models
 CV_MODELS_DIR = PROJECT_DIR / "models"
 # Path to dir with datasets
-CV_DATA_DIR = PROJECT_DIR / "data"
+CV_DATASETS_DIR = PROJECT_DIR / "datasets"
 # Path to dir with project artifacts
 CV_WORKAREA_DIR = PROJECT_DIR / "workarea"
