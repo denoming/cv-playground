43437b4c7744a40ca8c7a0b06bd6281d99ddf35d
Denys Asauliak <denoming@gmail.com>
2026-07-10 23:08:34 +0300

    feat: apply several optimizations
    + compile model
    + mixing precision
    + scaling gradients
    + fused optimizer
    + pin memory

--- common/torch/ops.py
+++ common/torch/ops.py
@@ -3,49 +3,56 @@ from tqdm.auto import tqdm
 from torch import nn
 from torch.utils import data, tensorboard
 from torch import optim
+from torch import amp
 from torchmetrics.metric import Metric
+from typing import Callable
 
-def train_step(model: nn.Module,
+
+def train_step(model: Callable,
                loader: data.DataLoader,
                optimizer: optim.Optimizer,
                criterion: nn.Module,
                metric: Metric,
+               scaler: amp.GradScaler,
                device: torch.device) -> tuple[float, float]:
     n_batches = len(loader)
     loss_avg, accu_avg = 0.0, 0.0
     metric.reset()
-    model.train()
     for idx, (x, y) in tqdm(enumerate(loader), total=n_batches):
-        x, y = x.to(device), y.to(device)
-        y_logits = model(x)
-        y_pred = torch.argmax(y_logits, dim=1)
-        loss = criterion(y_logits, y)
-        loss_avg += loss.item()
-        metric.update(y_pred, y)
         optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        x, y = x.to(device), y.to(device)
+        with torch.autocast(
+                device_type=device.type,
+                dtype=torch.float16,
+                enabled=scaler.is_enabled()):
+            logits = model(x)
+            loss = criterion(logits, y)
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        loss_avg += loss.item()
+        preds = torch.argmax(logits, dim=1)
+        metric.update(preds, y)
     loss_avg = loss_avg / n_batches
     accu_avg = metric.compute().item()
     return loss_avg, accu_avg
 
 
-def test_step(model: nn.Module,
+def test_step(model: Callable,
               loader: data.DataLoader,
               criterion: nn.Module,
               metric: Metric,
               device: torch.device) -> tuple[float, float]:
     n_batches = len(loader)
     loss_avg, accu_avg = 0.0, 0.0
-    model.eval()
     with torch.inference_mode():
         for idx, (x, y) in tqdm(enumerate(loader), total=len(loader)):
             x, y = x.to(device), y.to(device)
-            y_logits = model(x)
-            y_pred = torch.argmax(y_logits, dim=1)
-            loss = criterion(y_logits, y)
+            logits = model(x)
+            preds = torch.argmax(logits, dim=1)
+            loss = criterion(logits, y)
             loss_avg += loss.item()
-            metric.update(y_pred, y)
+            metric.update(preds, y)
     loss_avg = loss_avg / n_batches
     accu_avg = metric.compute().item()
     return loss_avg, accu_avg
@@ -56,15 +63,21 @@ def train(model: nn.Module,
           ts_dl: data.DataLoader,
           optimizer: torch.optim.Optimizer,
           criterion: nn.Module,
-          metric: Metric,
+          metric: Metric, 
           n_epochs: int,
           writer: tensorboard.SummaryWriter,
           device: torch.device,
+          use_amp: bool = True,
+          use_compile: bool = True,
           scheduler: optim.lr_scheduler.LRScheduler|None = None):
+    scaler = torch.amp.GradScaler(enabled=use_amp)
+    xmodel = torch.compile(model) if use_compile else model
     for epoch in range(1, n_epochs+1):
         print(f"> Epoch: {epoch:02}")
-        tr_loss, tr_accu = train_step(model, tr_dl, optimizer, criterion, metric, device)
-        ts_loss, ts_accu = test_step(model, ts_dl, criterion, metric, device)
+        model.train(mode=True)
+        tr_loss, tr_accu = train_step(xmodel, tr_dl, optimizer, criterion, metric, scaler, device)
+        model.train(mode=False)
+        ts_loss, ts_accu = test_step(xmodel, ts_dl, criterion, metric, device)
         if scheduler is not None:
             scheduler.step()
         writer.add_scalars(
--- common/torch/utils.py
+++ common/torch/utils.py
@@ -1,15 +1,95 @@
 import os
 import torch
+from pathlib import Path
+from typing import Any
 
 
-def get_default_device():
-    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def get_optimal_device_type() -> str:
+    return "cuda" if torch.cuda.is_available() else "cpu"
 
 
-def set_seed(seed: int = 0):
+def get_optimal_device():
+    return torch.device(get_optimal_device_type())
+
+
+def get_optimal_batch_size() -> int:
+    free_mem, _ = get_memory_info()
+    return 128 if free_mem >= 16.0 else 32
+
+
+def set_default_device() -> torch.device:
+    device = get_optimal_device()
+    torch.set_default_device(device)
+    return device
+
+
+def set_default_seed(seed: int = 0):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
 
 
+def set_default_optimizations():
+    if torch.cuda.is_available():
+        score = torch.cuda.get_device_capability()
+        if score >= (8, 0):
+            torch.set_float32_matmul_precision("high")
+        else:
+            torch.set_float32_matmul_precision("highest")
+
+
+def get_parameters_number(model: torch.nn.Module) -> int:
+    return sum(p.numel() for p in model.parameters())
+
+
+def get_memory_info(device: torch.device|None = None) -> tuple[float, float]:
+    if device is None:
+        device = get_optimal_device()
+    free_mem, total_mem = torch.cuda.mem_get_info(device)
+    return round(free_mem * 1e-9, 3), round(total_mem * 1e-9, 3)
+
+
+def save_model(model: torch.nn.Module,
+               target_dir: Path|str,
+               model_name: str):
+    """
+    Saves a PyTorch model to a target directory.
+
+    Args:
+    model: A target PyTorch model to save.
+    target_dir: A directory for saving the model to.
+    model_name: A filename for the saved model. Should include
+                either ".pth" or ".pt" as the file extension.
+    """
+    if isinstance(target_dir, str):
+        target_dir = Path(target_dir)
+    target_dir.mkdir(parents=True, exist_ok=True)
+
+    assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
+    model_path = target_dir / model_name
+    torch.save(obj=model.state_dict(), f=model_path)
+
+
+def export_model(model: torch.nn.Module,
+                 target_dir: Path|str,
+                 model_name: str,
+                 args: tuple[Any, ...] = (),
+                 version: int|None = None):
+    """
+    Export a PyTorch model to a target directory in ONNX format.
+
+    Args:
+    model: A target PyTorch model to save.
+    target_dir: A directory for saving the model to.
+    model_name: A filename for the saved model. Should include ".onnx" as the file extension.
+    args: A dummy inputs reflecting a production shape (e.g. (torch.zeros(1, 3, 224, 224),) if input is an RGB image)
+    version: A version of ONNX opset to use (default: None)
+    """
+    if isinstance(target_dir, str):
+        target_dir = Path(target_dir)
+    target_dir.mkdir(parents=True, exist_ok=True)
+
+    assert model_name.endswith(".onnx"), "model_name should end with '.onnx'"
+    model_path = target_dir / model_name
+    torch.onnx.export(model=model, args=args, f=model_path, opset_version=version)
